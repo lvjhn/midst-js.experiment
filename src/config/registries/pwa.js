0529b8d6baccb93ee3e130c05dde956e606cf467
LJ S.A <lvjhn.mx@gmail.com>
2022-11-03 23:37:24 +0800

    Added content in pwa registry

--- src/config/registries/pwa.js
+++ src/config/registries/pwa.js
@@ -3,5 +3,24 @@
  * Central registration file for PWA related setup such as service workers
  */
 export default async () => {
-
+    if(import.meta.env.PROD) {
+        if ("serviceWorker" in navigator) {
+            const updateSW = registerSW({
+                onNeedRefresh() {
+                    Toastify({
+                        text: `<div style="font-family: arial"> 
+                                <h4 style='display: inline;'>An update is available!</h4>
+                                <br><br>
+                                <a class='do-sw-update'>Click to update and reload</a>
+                            </div>`,
+                        escapeMarkup: false,
+                        gravity: "bottom",
+                        onClick() {
+                            updateSW(true);
+                        }
+                    }).showToast();
+                }
+            });
+        }
+    }
 }
